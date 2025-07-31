c6f5f8145f09bbee6ae6b17837b2b7a024bbf048
mukeshelumalai1991 <mukesh.elumalai1991@gmail.com>
2025-07-31 14:34:22 +0530

    upadated

--- StringConcatenation.js
+++ StringConcatenation.js
@@ -3,4 +3,4 @@ let str2="from ";
 let str3="having experience of "
 let str4 = `str1${Mukesh}+str2${Chennai}+str3${4.8}+"years in Automation Testing"`
 console.log("The concatenated String is ",str4);
-
+console.log("")
